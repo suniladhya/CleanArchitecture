b47ac037178a0c5da324c48e2ca228f1155c0864
Sunil Adhya <suniladhya@live.com>
2021-05-14 20:45:00 +0530

    CQRS initial Setup,Added MediatR Package

---
+++ CleanArch.Infra/Bus/InMemoryBus.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cleanArch.Infra.Bus
+{
+    public sealed class InMemoryBus
+    {
+        
+    }
+}
